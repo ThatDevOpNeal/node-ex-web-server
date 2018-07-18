b5ce2a5bd0af285d68bd32568e55eb3b0a740373
neal.bharucha@msichicago.org <nealbharucha@yahoo.com>
2018-07-18 16:57:31 -0500

    Add projects section to app.

--- server.js
+++ server.js
@@ -54,6 +54,14 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'Projects Page',
+        tabTitle: 'Projects',
+        bodyMessage: 'Portfolio Here'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
